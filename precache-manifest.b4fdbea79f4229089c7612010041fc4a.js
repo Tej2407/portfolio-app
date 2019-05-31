605d270a3f8bb8a2d6a8a2cf716af12ddb829a13
Tej2407 <tejhirpara1@gmail.com>
2019-05-31 19:07:20 +0530

    Updates

---
+++ precache-manifest.b4fdbea79f4229089c7612010041fc4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67d76a03939971eeced46ed587e2882",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "d93b0b429f66b0210cbd",
+    "url": "/portfolio-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d93b0b429f66b0210cbd",
+    "url": "/portfolio-app/static/js/main.6508820c.chunk.js"
+  },
+  {
+    "revision": "44340542c48e10c20cd6",
+    "url": "/portfolio-app/static/js/runtime~main.cb97d77a.js"
+  }
+]);
